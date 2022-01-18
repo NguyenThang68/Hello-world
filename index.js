9cbaebe18386bf38e61c3acc3d80e4ffaf2c9cc3
NguyenThang68 <it.thangnq@gmail.com>
2022-01-18 09:44:32 +0700

    update index

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World 2022 & Goodluck')
+  res.send('Hello World 2022 and Goodluck')
 })
 
 app.listen(port, () => {
